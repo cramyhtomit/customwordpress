0fccb896b17402f31862d8afb0c14b882c5e4304
Marc Timothy Ronquillo <marctimothyronquillo@gmail.com>
2023-04-09 22:50:39 +0300

    adding modifying page. php

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_files(){
+    wp_enqueue_script("university_main_js", get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style("university_googlefonts", 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,400;1,700&display=swap');
     wp_enqueue_style("university_fontawesome", 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css');
     wp_enqueue_style("university_main_styles", get_theme_file_uri('/build/style-index.css'));
