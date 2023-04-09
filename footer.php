db7666494420fcdb7d2bd292094cdc54a92407a5
Marc Timothy Ronquillo <marctimothyronquillo@gmail.com>
2023-04-09 21:40:34 +0300

    added font-awesome and changed <i> due to old files

--- footer.php
+++ footer.php
@@ -38,19 +38,19 @@
             <nav>
               <ul class="min-list social-icons-list group">
                 <li>
-                  <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a href="#" class="social-color-facebook"><i class="fa-brands fa-square-facebook"></i></i></a>
                 </li>
                 <li>
-                  <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  <a href="#" class="social-color-twitter"><i class="fa-brands fa-square-twitter" aria-hidden="true"></i></a>
                 </li>
                 <li>
-                  <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                  <a href="#" class="social-color-youtube"><i class="fa-brands fa-square-youtube" aria-hidden="true"></i></a>
                 </li>
                 <li>
-                  <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                  <a href="#" class="social-color-linkedin"><i class="fa-brands fa-linkedin" aria-hidden="true"></i></a>
                 </li>
                 <li>
-                  <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  <a href="#" class="social-color-instagram"><i class="fa-brands fa-square-instagram" aria-hidden="true"></i></a>
                 </li>
               </ul>
             </nav>
--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 
 function university_files(){
-    wp_enqueue_style("university_main_styles", get_stylesheet_uri());
+    wp_enqueue_style("university_fontawesome", 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css');
+    wp_enqueue_style("university_main_styles", get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style("university_extra_styles", get_theme_file_uri('/build/index.css'));
+
+    
 }
 
 add_action( "wp_enqueue_scripts", "university_files");
